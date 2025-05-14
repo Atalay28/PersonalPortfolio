a8278da568259ab1f999c13b17bfcb8a293d7d49
Atalay Muhsin Baysal <basamak2012@hotmail.com>
2025-05-14 19:03:07 +0300

    1. commit
    Started main page, write scripts and made styles

---
+++ scripts/giris.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var moreAboutMeButton = document.querySelector(".button button");
+  
+    moreAboutMeButton.addEventListener("click", function () {
+      var about2Section = document.querySelector("#about2");
+      about2Section.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    var chessbutton=document.getElementById("chessmatch");
+    chessbutton.addEventListener("click",function(){
+        window.location.href="https://www.youtube.com/watch?v=-AH7bw_5zPo";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var moreAboutMeButton = document.querySelector(".about4 .button button");
+  
+    moreAboutMeButton.addEventListener("click", function () {
+      window.location.href =
+        "https://youtu.be/PCiWTRg-itY";
+    });
+  });
+  
